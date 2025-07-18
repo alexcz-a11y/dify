35e40b576920ab822aa718499e627f2abde4a6fb
stream <stream@dify.ai>
2025-07-18 15:43:37 +0800

    feat: improved capability with parameters of InstructionGenerateApi

--- api/core/llm_generator/llm_generator.py
+++ api/core/llm_generator/llm_generator.py
@@ -410,7 +410,8 @@ class LLMGenerator:
         flow_id: str,
         current: str,
         instruction: str,
-        model_config: dict
+        model_config: dict,
+        ideal_output: str | None
     ) -> dict:
         app: App = db.session.query(App).filter(App.id == flow_id).first()
         last_run: Message = (db.session.query(Message)
@@ -421,7 +422,7 @@ class LLMGenerator:
             return LLMGenerator.__instruction_modify_common(
                 tenant_id=tenant_id,
                 model_config=model_config,
-                last_run={ "error": "This hasn't been run" },
+                last_run=None,
                 current=current,
                 error_message="",
                 instruction=instruction,
@@ -440,6 +441,7 @@ class LLMGenerator:
             error_message=str(last_run.error),
             instruction=instruction,
             node_type="llm",
+            ideal_output=ideal_output
         )
 
     @staticmethod
@@ -449,7 +451,8 @@ class LLMGenerator:
         node_id: str,
         current: str,
         instruction: str,
-        model_config: dict
+        model_config: dict,
+        ideal_output: str | None
     ) -> dict:
         from services.workflow_service import WorkflowService
         app: App = db.session.query(App).filter(App.id == flow_id).first()
@@ -465,11 +468,12 @@ class LLMGenerator:
             return LLMGenerator.__instruction_modify_common(
                 tenant_id=tenant_id,
                 model_config=model_config,
-                last_run={ "error": "This hasn't been run" },
+                last_run=None,
                 current=current,
                 error_message="",
                 instruction=instruction,
                 node_type="llm",
+                ideal_output=ideal_output
             )
 
         def agent_log_of(node_execution: WorkflowNodeExecutionModel) -> Sequence:
@@ -483,7 +487,7 @@ class LLMGenerator:
                     "error": event.error,
                     "data": event.data,
                 }
-            return json.dumps(map(dict_of_event, parsed))
+            return [dict_of_event(event) for event in parsed]
         last_run_dict = {
             "inputs": last_run.inputs_dict,
             "status": last_run.status,
@@ -499,6 +503,7 @@ class LLMGenerator:
             error_message=last_run.error,
             instruction=instruction,
             node_type=last_run.node_type,
+            ideal_output=ideal_output
         )
 
 
@@ -507,11 +512,12 @@ class LLMGenerator:
     def __instruction_modify_common(
         tenant_id: str,
         model_config: dict,
-        last_run: dict,
+        last_run: dict | None,
         current: str,
-        error_message: str,
+        error_message: str | None,
         instruction: str,
         node_type: str,
+        ideal_output: str | None
     ) -> dict:
         LAST_RUN = "{{#last_run#}}"
         CURRENT = "{{#current#}}"
@@ -543,6 +549,7 @@ class LLMGenerator:
                     "current": current,
                     "last_run": last_run,
                     "instruction": injected_instruction,
+                    "ideal_output": ideal_output,
                 }
             ))
         ]
